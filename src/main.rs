6e0bdc669f7ab80c601b486a4d8bca2b66603a10
persona-mp3 <randomnobscurebs@gmail.com>
2026-04-18 22:58:58 +0100

    feat: implemnted simple coordinator with multithreaded clients

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod coordinator;
 mod worker;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -5,65 +6,24 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // multithreaded. This is supposed to simulate a worker
     // asking the co-ordinator main for a task, and when they also report they're done w a task. But for now, we're asking for
     // a specific task
-    let task_file_path = get_task("tasks/the_hemingway.txt", false);
+    // let task_file_path = get_task("tasks/the_hemingway.txt", false);
     {
         let _rpc_response = worker::ask_task();
     }
 
-    worker::worker(&task_file_path, custom_map, custom_reduce)?;
+    // worker::worker(&task_file_path, custom_map, custom_reduce)?;
+    let current_tasks = vec![
+        coordinator::Task::new(String::from("tasks/the_hemingway.txt")),
+        coordinator::Task::new(String::from("tasks/carvan.txt")),
+        coordinator::Task::new(String::from("tasks/poem.txt")),
+    ];
+
+    let _ = coordinator::coordinator(current_tasks, custom_map, custom_reduce);
     Ok(())
 }
 
-#[derive(PartialEq, Debug)]
-enum State {
-    Idle = 0,
-    InProgress = 1,
-    Done = 2,
-}
-
-#[derive(Debug)]
-struct Task {
-    file_name: String,
-    state: State,
-}
-
-fn get_task(task_name: &str, ask: bool) -> String {
-    println!("[CO-ORDINATOR] Getting task");
-    let mut current_tasks = vec![
-        Task {
-            file_name: String::from("tasks/the_hemingway.txt"),
-            state: State::Idle,
-        },
-        Task {
-            file_name: String::from("tasks/carvan.txt"),
-            state: State::Idle,
-        },
-        Task {
-            file_name: String::from("tasks/poem.txt"),
-            state: State::Idle,
-        },
-    ];
-
-    for task in &mut current_tasks {
-        if task.file_name == task_name && ask && task.state == State::InProgress {
-            task.state = State::Done;
-            break;
-        }
-    }
-
-    for task in &mut current_tasks {
-        if task.state == State::Idle {
-            task.state = State::InProgress;
-            println!("[CO-ORDINATOR] Assigning task: {:?}", task);
-            return task.file_name.clone();
-        }
-    }
-
-    String::from("")
-}
-
 fn custom_map(key: &String, value: &str) -> Vec<worker::MKeyValue> {
-    println!("[USER_MAP]: Starting implementation for {}", key);
+    println!("[map]: {}", key);
 
     let mut cleaned_content: String = String::with_capacity(value.len());
 
--- src/worker.rs
+++ src/worker.rs
@@ -1,5 +1,7 @@
 use std::collections::HashMap;
-use std::fs;
+use std::sync::mpsc::Sender;
+use std::thread::JoinHandle;
+use std::{fs, thread};
 
 #[derive(Debug)]
 pub struct MKeyValue {
@@ -27,20 +29,28 @@ struct HashCounter {
     values: Vec<String>,
 }
 
+#[derive(Debug)]
+pub struct WorkerInstruction {
+    pub send_ch: Sender<Option<Vec<MKeyValue>>>,
+    pub file_path: String,
+    pub map_fn: MapFn,
+    pub reduce_fn: ReduceFn,
+}
+
 /// Worker function
 pub fn worker(
-    task_file_name: &String,
+    task_file_path: &String,
     map_fn: MapFn,
     reduce_fn: ReduceFn,
 ) -> Result<Vec<MKeyValue>, Box<dyn std::error::Error>> {
     println!("[WORKER] Woker starting");
-    let content = match fs::read_to_string(task_file_name) {
+    let content = match fs::read_to_string(task_file_path) {
         Ok(v) => v,
         Err(err) => {
             let err_info = format!(
                 "[WORKER] could not open task_file: {}
                 Reason: {err}",
-                task_file_name
+                task_file_path
             );
             return Err(err_info.into());
         }
@@ -53,7 +63,7 @@ pub fn worker(
     // I'm not sure yet, but I think I can wayne on the second one, since it almost
     // looks like the function signature described in the GO code
     // let words = content.split_ascii_whitespace();
-    let list_kv_pairs = map_fn(task_file_name, &content);
+    let list_kv_pairs = map_fn(task_file_path, &content);
 
     let mut tally: HashMap<String, HashCounter> = HashMap::with_capacity(list_kv_pairs.len());
     for kv in list_kv_pairs.iter() {
@@ -78,7 +88,40 @@ pub fn worker(
     }
 
     for (k, v) in tally {
-        println!("{}", reduce_fn(k, v.values))
+        let _ = reduce_fn(k, v.values);
     }
+
+    println!("[worker] done with {task_file_path}");
     Ok(list_kv_pairs)
+}
+
+pub fn thread_worker(instruction: WorkerInstruction) -> JoinHandle<()>{
+    let handle = thread::spawn(move || {
+        let result = worker(
+            &instruction.file_path,
+            instruction.map_fn,
+            instruction.reduce_fn,
+        );
+
+        let _ = match result {
+            Ok(success) => {
+                instruction
+                    .send_ch
+                    .send(Some(success))
+                    .expect("[worker] could not send. orphaned. Coordinator has been closed");
+                drop(instruction.send_ch);
+            }
+            Err(err) => {
+                println!("[worker-error]: {err}",);
+                instruction
+                    .send_ch
+                    .send(None)
+                    .expect("[worker] could not send. orphaned. Coordinator has been closed");
+                drop(instruction.send_ch);
+                return;
+            }
+        };
+    });
+
+    handle
 }
